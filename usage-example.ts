d0eecedd539197a10b354ac3b56fbd302a2220e7
Monique Baumann <monique@freedomcash.org>
2024-07-03 12:49:14 +0200

    free the world

--- usage-example.ts
+++ usage-example.ts
@@ -11,7 +11,7 @@ const amountOfBaseCurrencyToKeepInEachWallet = Deno.args[6]
 console.log(amountOfBaseCurrencyToKeepInEachWallet)
 if (minSleep == undefined || maxSleep == undefined || minSleep < 9) {
     throw new Error(`you might consider sleeping when the time is right`)
-} else if (minAmount == undefined || maxAmount == undefined || amountOfBaseCurrencyToKeepInEachWallet == undefined || minAmount < 1 || maxAmount > 9) {
+} else if (minAmount == undefined || maxAmount == undefined || amountOfBaseCurrencyToKeepInEachWallet == undefined || minAmount < 1 || maxAmount > 9000000000000000000) {
     throw new Error(`you might consider experimenting with specific small amounts`)
 } else if (providerURL === undefined || pkTestWallet === undefined) {
     throw new Error("configuration parameter missing")
